7a812854270414be9e496c053dd9a2015d8640bc
Timo Benkhard <tcbenkhard@gmail.com>
2020-12-02 08:41:29 +0100

    Day 2 solution

---
+++ Day 2/src/main/java/nl/benkhard/aoc/Main.java
@@ -0,0 +1,28 @@
+package nl.benkhard.aoc;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> lines = FileUtils.readFileAsListOfStrings(Main.class.getClassLoader(), "input.txt");
+        int validPasswords = 0;
+        for(String line : lines) {
+            String[] splitLine = line.split(": ");
+            if(isValid(splitLine[0], splitLine[1]))
+                validPasswords++;
+        }
+
+        System.out.println(String.format("%s valid passwords", validPasswords));
+    }
+
+    public static boolean isValid(String policyString, String password) {
+        String[] policy = policyString.split(" ");
+        String[] minMax = policy[0].split("-");
+        int min = Integer.valueOf(minMax[0]);
+        int max = Integer.valueOf(minMax[1]);
+        char character = policy[1].charAt(0);
+
+        return password.charAt(min-1) == character ^ password.charAt(max-1) == character;
+    }
+}
--- src/main/java/nl/benkhard/aoc/FileUtils.java
+++ src/main/java/nl/benkhard/aoc/FileUtils.java
@@ -6,12 +6,21 @@ import java.util.Scanner;
 
 public class FileUtils {
 
-    public static List<Integer> readFileAsListOfIntegers(ClassLoader loader, String filename) {
+    public static List<Integer> readFileAsListOfIntegers(ClassLoader  loader, String filename) {
         Scanner scanner = new Scanner(loader.getResourceAsStream(filename));
         List<Integer> inputNumbers = new ArrayList<Integer>();
         while(scanner.hasNext()) {
             inputNumbers.add(scanner.nextInt());
         }
         return inputNumbers;
+    }
+
+    public static List<String> readFileAsListOfStrings(ClassLoader loader, String filename) {
+        Scanner scanner = new Scanner(loader.getResourceAsStream(filename));
+        List<String> inputStrings = new ArrayList();
+        while(scanner.hasNext()) {
+            inputStrings.add(scanner.nextLine());
+        }
+        return inputStrings;
     }
 }
